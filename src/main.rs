ddd22b9d825cc89f51f69f9f58baf767c465681f
matiqo1234r <madseries321+github@gmail.com>
2024-06-02 14:20:29 +0200

    chore: Update .gitignore and Cargo.toml, add SSH2 dependency, and implement SSH command execution to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io::Read;
+use ssh2::Session;
+
 fn main() {
-    println!("Hello, world!");
+    let user = "USERNAME HERE";
+    let host = "HOST HERE (IP or domain)";
+    let passwd = "PASSWORD TO USER HERE";
+
+    let tcp = std::net::TcpStream::connect(format!("{}:22", host)).unwrap();
+    let mut sess = Session::new().unwrap();
+    sess.set_tcp_stream(tcp);
+    sess.handshake().unwrap();
+    sess.userauth_password(user, passwd).unwrap();
+    let mut channel = sess.channel_session().unwrap();
+
+    channel.exec("ls").unwrap(); // Runs a ls command
+    let mut output = Vec::new();
+
+    channel.read_to_end(&mut output).unwrap();
+    println!("{}", String::from_utf8_lossy(&output));
 }
