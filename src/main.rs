4d849ae154aeca30f346066b15d9fb00a227ce28
matiqo1234r <madseries321+github@gmail.com>
2024-06-02 14:31:21 +0200

    Add fire function to main.rs for text visualization

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,40 @@
+use std::process::Command;
 use std::io::Read;
 use ssh2::Session;
 
+fn fire(text: &str) -> String {
+    let _ = Command::new("clear").status();
+
+    let mut faded = String::new();
+    let mut green = 250;
+
+    for line in text.lines() {
+        faded.push_str(&format!("\x1b[38;2;255;{};0m{}\x1b[0m\n", green, line));
+        if green > 0 {
+            green -= 25;
+        }
+    }
+    faded
+}
+
 fn main() {
+    let message = r#"
+      ██▒   █▓ ███▄ ▄███▓    ███▄    █  █    ██  ██ ▄█▀▓█████  ██▀███  
+     ▓██░   █▒▓██▒▀█▀ ██▒    ██ ▀█   █  ██  ▓██▒ ██▄█▒ ▓█   ▀ ▓██ ▒ ██▒
+      ▓██  █▒░▓██    ▓██░   ▓██  ▀█ ██▒▓██  ▒██░▓███▄░ ▒███   ▓██ ░▄█ ▒
+       ▒██ █░░▒██    ▒██    ▓██▒  ▐▌██▒▓▓█  ░██░▓██ █▄ ▒▓█  ▄ ▒██▀▀█▄  
+        ▒▀█░  ▒██▒   ░██▒   ▒██░   ▓██░▒▒█████▓ ▒██▒ █▄░▒████▒░██▓ ▒██▒
+        ░ ▐░  ░ ▒░   ░  ░   ░ ▒░   ▒ ▒ ░▒▓▒ ▒ ▒ ▒ ▒▒ ▓▒░░ ▒░ ░░ ▒▓ ░▒▓░
+        ░ ░░  ░  ░      ░   ░ ░░   ░ ▒░░░▒░ ░ ░ ░ ░▒ ▒░ ░ ░  ░  ░▒ ░ ▒░
+          ░░  ░      ░         ░   ░ ░  ░░░ ░ ░ ░ ░░ ░    ░     ░░   ░ 
+           ░         ░               ░    ░     ░  ░      ░  ░   ░     
+          ░                                  
+                                       ＢＹ ＭＡＴＩＸＡＮＤＲ０９                     
+"#;
+
+    let fire_text = fire(message);
+    println!("{}", fire_text);
+
     let user = "USERNAME HERE";
     let host = "HOST HERE (IP or domain)";
     let passwd = "PASSWORD TO USER HERE";
